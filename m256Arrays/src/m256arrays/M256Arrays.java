d8882a79fac3a97529351d025aba21a6fa96ac36
Jack Pincombe <jackpincombe1@gmail.com>
2017-03-27 17:54:40 +0100

    second commit

---
+++ m256Arrays/src/m256arrays/M256Arrays.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m256arrays;
+
+/**
+ *
+ * @author jackpincombe
+ */
+public class M256Arrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    int[] myList = {1,2,3,4,5,6,6,8,6,4};
+    
+    for (int i = 0; i < myList.length; i++) {
+        System.out.println(myList[i] + " ");
+    }
+    
+    
+    
+    
+            }
+    
+}
