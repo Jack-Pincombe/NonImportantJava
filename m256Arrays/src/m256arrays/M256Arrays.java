711e7c029332dce524947ea928872e606ad05e9f
Jack Pincombe <jackpincombe1@gmail.com>
2017-03-27 18:32:03 +0100

    Implemented bubble sort with java

--- m256Arrays/src/m256arrays/M256Arrays.java
+++ m256Arrays/src/m256arrays/M256Arrays.java
@@ -19,6 +19,28 @@ public class M256Arrays {
     
     
     int[] myList = {1,2,3,4,5,6,6,8,6,4};
+    boolean sort = true;
+    int tmp;
+    
+    while (sort){
+        sort = false;
+        for (int i = 0; i < myList.length - 1; i++)
+        {
+         
+            
+            if (myList[i] > myList[i + 1])
+            {
+                tmp = myList[i];
+                myList[i] = myList[i + 1];
+                myList[i + 1] = tmp;
+                sort = true;
+                
+            }
+        }
+        
+    } 
+    
+    System.out.println(myList);
     
     for (int i = 0; i < myList.length; i++) {
         System.out.println(myList[i] + " ");
@@ -27,6 +49,6 @@ public class M256Arrays {
     
     
     
-            }
+    }
     
 }
